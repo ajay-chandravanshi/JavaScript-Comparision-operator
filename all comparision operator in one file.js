0628887b194d0da429a3aae21f8fc55142fb5dd5
ajay-chandravanshi <ajaystudy38@gmail.com>
2024-12-26 02:40:27 +0530

    new code

---
+++ all comparision operator in one file.js
@@ -0,0 +1,39 @@
+var a=20;
+var b=10;
+var c= a>b;
+console.log(c)
+
+var a=20;
+var b=10;
+var c= a<b;
+console.log(c)
+
+var a=20;
+var b=30;
+var c= a>=b;
+console.log(c)
+
+var a=20;
+var b=10;
+var c= a<=b;
+console.log(c)
+
+var a=20;
+var b=10;
+var c=a==b;
+console.log(c)
+
+var a=20;
+var b=10;
+var c= a!=b;
+console.log(c)
+
+var a = 20;
+var b = 20;
+var c = a == b;
+console.log(c)
+
+var a=20;
+var b=10;
+var c=a!==b;
+console.log(c)
